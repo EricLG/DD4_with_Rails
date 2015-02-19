9bc523e8ffb63f54f3352a3c634bfe98d70c9846
Eric <eric.le.guellaut@hotmail.fr>
2015-02-19 20:51:53 +0100

    Correction relations

--- dd4/app/models/weapon_group.rb
+++ dd4/app/models/weapon_group.rb
@@ -1,5 +1,4 @@
 class WeaponGroup < ActiveRecord::Base
   has_and_belongs_to_many :common_weapons
-  has_and_belongs_to_many :common_armors
   has_and_belongs_to_many :magic_weapons
 end
--- dd4/app/models/weapon_property.rb
+++ dd4/app/models/weapon_property.rb
@@ -1,4 +1,3 @@
 class WeaponProperty < ActiveRecord::Base
   has_and_belongs_to_many :common_weapons
-  has_and_belongs_to_many :common_armors
 end
