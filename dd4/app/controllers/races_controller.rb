d40346b40a3d9aa8cf735418f14c8e469b801da6
Eric <eric.le.guellaut@hotmail.fr>
2015-02-20 23:12:11 +0100

    Ajout affichage sommaire classes et races

--- dd4/app/controllers/races_controller.rb
+++ dd4/app/controllers/races_controller.rb
@@ -1,4 +1,7 @@
 class RacesController < ApplicationController
+
   def index
+    @races =["Changelin","Cristallien","Demi-elfe","Demi-orque","Deva","Drakeide","Drow","Eladrin","Elfe","Feral","Forgelier","Genasi","Githzerai","Gnome","Goliath","Halfelin","Humain","Kalashtar","Minotaure","Nain","Sylvien","Tieffelin"]
   end
+
 end
