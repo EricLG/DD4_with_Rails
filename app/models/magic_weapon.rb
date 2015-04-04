a48b74c25c2981ef6c95e3255613d8dc4edfc79e
Eric <eric.le.guellaut@hotmail.fr>
2015-04-04 11:22:27 +0200

    Indentation

--- app/models/magic_weapon.rb
+++ app/models/magic_weapon.rb
@@ -20,35 +20,35 @@ class MagicWeapon < ActiveRecord::Base
     filename = Dir.entries('tmp/import_files').find{|f| f.match(/export_arme_OK/)}
     unless filename.nil?
       ActiveRecord::Base.transaction do
-      File.open(File.join('tmp/import_files', filename), 'r') do |f|
-        f.readline
-        f.each_line do |l| # "Titre";"Description";"Altération";"Niveau minimum";"Prix par niveau et altération";"Arme";"Critique";"Propriété";"Pouvoir";"Source";"Spécial"
-          array_line = l.split(/";"/, -1)
-          m = MagicWeapon.new(
-            name:           ImportData.clear_field(array_line[0]),
-            description:    ImportData.clear_field(array_line[1]),
-            alteration:     ImportData.clear_field(array_line[2]),
-            critical:       ImportData.clear_field(array_line[6]),
-            property:       ImportData.clear_field(array_line[7]),
-            power:          ImportData.clear_field(array_line[8]),
-            special:        ImportData.clear_field(array_line[10]),
-            source:         sources.find{|s| s.name == ImportData.clear_field(array_line[9])},
-            object_levels:  ImportData.calcul_level_array(ImportData.clear_field(array_line[3]).to_i, levels),
-            weapon_groups:  ImportData.find_weapon_groups(ImportData.clear_field(array_line[5]), groups)
-            )
-          if m.valid?
-            begin
-              m.save!
-            rescue
-              logger.debug "Erreur lors de l'ajout de l'arme suivante: #{m.name}"
-              logger.debug "#{m.errors.full_messages}"
+        File.open(File.join('tmp/import_files', filename), 'r') do |f|
+          f.readline
+          f.each_line do |l| # "Titre";"Description";"Altération";"Niveau minimum";"Prix par niveau et altération";"Arme";"Critique";"Propriété";"Pouvoir";"Source";"Spécial"
+            array_line = l.split(/";"/, -1)
+            m = MagicWeapon.new(
+              name:           ImportData.clear_field(array_line[0]),
+              description:    ImportData.clear_field(array_line[1]),
+              alteration:     ImportData.clear_field(array_line[2]),
+              critical:       ImportData.clear_field(array_line[6]),
+              property:       ImportData.clear_field(array_line[7]),
+              power:          ImportData.clear_field(array_line[8]),
+              special:        ImportData.clear_field(array_line[10]),
+              source:         sources.find{|s| s.name == ImportData.clear_field(array_line[9])},
+              object_levels:  ImportData.calcul_level_array(ImportData.clear_field(array_line[3]).to_i, levels),
+              weapon_groups:  ImportData.find_weapon_groups(ImportData.clear_field(array_line[5]), groups)
+              )
+            if m.valid?
+              begin
+                m.save!
+              rescue
+                logger.debug "Erreur lors de l'ajout de l'arme suivante: #{m.name}"
+                logger.debug "#{m.errors.full_messages}"
+              end
+            else
+               logger.debug "Erreur de validation sur l'arme #{m.name}"
+               logger.debug "#{m.errors.full_messages}"
             end
-          else
-             logger.debug "Erreur de validation sur l'arme #{m.name}"
-             logger.debug "#{m.errors.full_messages}"
           end
         end
-  end
       end
     end
     c = MagicWeapon.count
