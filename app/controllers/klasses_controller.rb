3377294a03faa471540e020364aa9d76b67056da
Eric <eric.le.guellaut@hotmail.fr>
2015-07-06 19:32:00 +0200

    local feat + divers modifs

--- app/controllers/klasses_controller.rb
+++ app/controllers/klasses_controller.rb
@@ -1,10 +1,12 @@
 class KlassesController < ApplicationController
 
   def index
+    @hide_side_bloc = true
     @klasses = Klass.select(:id, :name).all
   end
 
   def show
+    @klasses = Klass.select(:id, :name).all
     @klass = Klass.includes(:klass_features, :source).find_by_id(params[:id])
   end
 
--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -1,10 +1,12 @@
 class RacesController < ApplicationController
 
   def index
+    @hide_side_bloc = true
     @races = Race.select(:id, :name).all
   end
 
   def show
+    @races = Race.select(:id, :name).all
     @race = Race.includes(:race_features, :source).find_by_id(params[:id])
   end
 
