62acd4e91b3031016a0482bb91930f09346bc62c
Eric <eric.le.guellaut@hotmail.fr>
2015-07-07 15:27:43 +0200

    Correction champ email et password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:user][:email])
+    email = params[:user][:email].try(:downcase)
+    user = User.find_by_email(email)
     # If the user exists AND the password entered is correct.
     if user && user.authenticate(params[:user][:password])
       # Save the user id inside the browser cookie. This is how we keep the user
