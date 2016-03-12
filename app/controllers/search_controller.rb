6040701e9866e30f42d9d2a3350b440826ee6ea2
Eric <eric.le.guellaut@hotmail.fr>
2016-03-12 14:20:49 +0100

    La barre de recherche recherche maintenant dans les talents (nom et avantage)

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,13 +4,16 @@ class SearchController < ApplicationController
     @hide_side_bloc = true
     @request = params[:search]
     @result = {}
-    @result[:magic_weapon], @result[:magic_armor], @result[:magic_implement], @result[:magic_gear], @total = [], [], [], [], 0
+    @result[:magic_weapon], @result[:magic_armor], @result[:magic_implement], @result[:magic_gear], @result[:magic_gear] = [], [], [], [], []
+    @total = 0
     results = MagicItem.where('magic_items.name ILIKE ? or property ILIKE ? or power ILIKE ?', "%#{@request}%", "%#{@request}%", "%#{@request}%")
+    f_results = Feat.where('name ILIKE ? or avantage ILIKE ?', "%#{@request}%", "%#{@request}%")
     unless @request.blank?
       @result[:magic_weapon ]     = results.weapons
       @result[:magic_armor ]      = results.armors
       @result[:magic_implement ]  = results.implements
       @result[:magic_gear ]       = results.gears
+      @result[:feat ]       = f_results
       @total = count_result(@result)
       flash.now[:error] = "Aucun résultats" if @total == 0
     end
