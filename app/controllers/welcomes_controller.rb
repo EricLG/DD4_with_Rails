a043ad714d43ba0efd404526a9b27a4a114c5f79
Eric <eric.le.guellaut@hotmail.fr>
2016-03-17 21:03:48 +0100

    Ajout page de statistiques basique

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -40,4 +40,30 @@ class WelcomesController < ApplicationController
       redirect_to feats_path
     end
   end
+
+  def stats
+    @stats = []
+    @stats << {table_name: "ArmorCategory",    table_count: ArmorCategory.count}
+    @stats << {table_name: "Campaign",         table_count: Campaign.count}
+    @stats << {table_name: "Character",        table_count: Character.count}
+    @stats << {table_name: "CommonArmor",      table_count: CommonArmor.count}
+    @stats << {table_name: "CommonWeapon",     table_count: CommonWeapon.count}
+    @stats << {table_name: "Consumable",       table_count: Consumable.count}
+    @stats << {table_name: "Feat",             table_count: Feat.count}
+    @stats << {table_name: "Game",             table_count: Game.count}
+    @stats << {table_name: "ImplementGroup",   table_count: ImplementGroup.count}
+    @stats << {table_name: "Klass",            table_count: Klass.count}
+    @stats << {table_name: "KlassFeature",     table_count: KlassFeature.count}
+    @stats << {table_name: "Location",         table_count: Location.count}
+    @stats << {table_name: "MagicItem",        table_count: MagicItem.count}
+    @stats << {table_name: "ObjectLevel",      table_count: ObjectLevel.count}
+    @stats << {table_name: "Race",             table_count: Race.count}
+    @stats << {table_name: "RaceFeature",      table_count: RaceFeature.count}
+    @stats << {table_name: "Source",           table_count: Source.count}
+    @stats << {table_name: "Stat",             table_count: Stat.count}
+    @stats << {table_name: "User",             table_count: User.count}
+    @stats << {table_name: "WeaponCategory",   table_count: WeaponCategory.count}
+    @stats << {table_name: "WeaponGroup",      table_count: WeaponGroup.count}
+    @stats << {table_name: "WeaponProperty",   table_count: WeaponProperty.count}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -54,6 +54,7 @@ Rails.application.routes.draw do
       get :feats
       get :contribute
       get :random
+      get :stats
     end
   end
   # The priority is based upon order of creation: first created -> highest priority.
