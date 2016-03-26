eb93d4e65e96e81fb4b1925c4feff5f96dcce1fc
Eric <eric.le.guellaut@hotmail.fr>
2016-03-26 12:54:26 +0100

    Correction relations magic items

--- app/models/armor_category.rb
+++ app/models/armor_category.rb
@@ -1,6 +1,6 @@
 class ArmorCategory < ActiveRecord::Base
   has_many :common_armors
-  has_and_belongs_to_many :magic_armors
+  has_and_belongs_to_many :magic_items
 
   def is_shield?
     self.code == 'lightsh' || self.code == 'heavysh'
