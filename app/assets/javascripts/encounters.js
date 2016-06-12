5a1ce103d014f3b02379b2fbce22bad4e3689f4a
Eric <eric.le.guellaut@hotmail.fr>
2016-06-12 16:11:19 +0200

    MAJ html css et ajout du filtre par niveau +-5

--- app/assets/javascripts/encounters.js
+++ app/assets/javascripts/encounters.js
@@ -2,17 +2,9 @@
 // All this logic will automatically be available in application.js.
 $(document).on("page:change", function() {
 
-  $(".budget").change(function() {
-    $("#budget_form").submit();
-  });
-
-  $(".select_form").change(function() {
-    $("#new_monster_search").submit();
-  });
-
-  $('#budget_form').on('ajax:success', function(event, xhr, status, error) {
-    $("#encounter_budget").text(xhr.budget);
-  });
+  // Initialisation
+  $("#budget_form").submit();
+  $("#new_monster_search").submit();
 
   $( ".select-monster" ).select2({
     theme: "bootstrap",
@@ -20,14 +12,33 @@ $(document).on("page:change", function() {
     allowClear: true,
   });
 
+  // Budget form
+  $(".budget").change(function() {
+    $("#budget_form").submit();
+  });
 
+  $("#party_level").change(function() {
+    $("#monster_search_level").val($("#party_level").val());
+    $("#new_monster_search").submit();
+  });
+
+  $('#budget_form').on('ajax:success', function(event, xhr, status, error) {
+    $("#encounter_budget").text(xhr.budget);
+  });
+
+  // filter monster form
+  $(".select_form").change(function() {
+    $("#new_monster_search").submit();
+  });
+
+  //update the select2 options list
   $('#new_monster_search').on('ajax:success', function(event, xhr, status, error) {
-    $( ".select-monster").empty();
-    $( ".select-monster").append($("<option value=\"\"></option>"));
+    $( ".select-monster").empty().append($("<option value=\"\"></option>"));
     $.each(xhr, function(key, value) {
       $( ".select-monster" ).append($("<option></option>").attr("value", value.id).text("Niv. " + value.level + " | " +value.name));
     });
 
     $( ".select-monster" ).trigger('change');
   });
+
 });
--- app/controllers/dm_tools/monsters_controller.rb
+++ app/controllers/dm_tools/monsters_controller.rb
@@ -75,6 +75,10 @@ class DmTools::MonstersController < ApplicationController
       monsters = monsters.where(second_role: params[:monster_search][:second_role]) unless params[:monster_search][:second_role].empty?
       monsters = monsters.where(race: params[:monster_search][:race]) unless params[:monster_search][:race].empty?
       monsters = monsters.where(leader: params[:monster_search][:leader]) unless params[:monster_search][:leader].empty?
+      unless params[:monster_search][:level].empty?
+        level = params[:monster_search][:level].to_i
+        monsters = monsters.where(level: level-5..level+5)
+      end
     end
     monsters = monsters.order(level: :asc, name: :asc)
 
