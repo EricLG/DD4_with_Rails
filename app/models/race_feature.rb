c7dad8c2db50824db71e8e28d5b124c55cbac2c3
Eric <eric.le.guellaut@hotmail.fr>
2016-10-01 21:56:59 +0200

    Correction affichage aptitude raciales suite complement genasi et silvyen

--- app/models/race_feature.rb
+++ app/models/race_feature.rb
@@ -6,4 +6,9 @@ class RaceFeature < ActiveRecord::Base
   has_and_belongs_to_many :prerequisite_for_feats, :class_name => "Feat", :join_table => :pr_race_features_for_feat
   has_many :character_choices
   has_many :characters, through: :character_choices
+
+
+  scope :main_ft,       -> {where(parent_feature_id: nil).order(:name)}
+  scope :required,      -> {where(required: "required") }
+  scope :first_choice,  -> {where(required: "choice_1") }
 end
