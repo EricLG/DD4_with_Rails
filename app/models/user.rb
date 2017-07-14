c62a51f31150ddf35e1576f0f2e02b2f1bbeac20
Eric <eric.le.guellaut@hotmail.fr>
2017-07-14 17:58:29 +0200

    Ajout des relations campaigns players users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
 
   has_many :characters
-  has_secure_password
 
   has_many :wishlists
   has_many :magic_items , through: :wishlists
+  has_many :players
+  has_many :campaigns, through: :players
+
+  has_secure_password
 end
