e476d3a085faada2149dd50ed57cb25cd1329eef
Eric <eric.le.guellaut@hotmail.fr>
2017-07-21 22:47:46 +0200

    L'admin peut modifier des users (nom et mot de passe)

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,6 +5,20 @@ class Admin::UsersController < ApplicationController
     @users = User.all.order(:created_at)
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admin_users_path
+    else
+      flash[:error] = "Erreur sur les champs suivants: #{@user.errors.full_messages}"
+      render :edit
+    end
+  end
+
   def destroy
     user = User.find(params[:id])
     user.destroy
@@ -12,4 +26,9 @@ class Admin::UsersController < ApplicationController
     redirect_to admin_users_path
   end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password)
+  end
 end
