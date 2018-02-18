35336c9032c16c5ad4665cf6174df79fcdf8b7b3
Eric <eric.le.guellaut@hotmail.fr>
2018-02-18 14:20:32 +0100

    Correction affichage des monstres + import monstes

--- app/helpers/monsters_helper.rb
+++ app/helpers/monsters_helper.rb
@@ -1,2 +1,6 @@
 module MonstersHelper
+
+  def custom_transliterate(source)
+    return source.mb_chars.downcase.to_s.gsub(/[Ééêè]/, 'e').gsub(/[\s]/, '_').gsub(/[\î]/, 'i').gsub(/[âà]/, 'a').gsub(/[\ô]/, 'o').gsub(/[Œœ]/, 'oe').gsub(/['-]/, '_').gsub(/[,]/, '').gsub(/[ûù]/, 'u')
+  end
 end
