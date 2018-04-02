efb4b5e660eda60f7e6c27f3922d717b79553a91
Eric <eric.le.guellaut@hotmail.fr>
2018-04-02 21:50:26 +0200

    suppresion ref variable d'instance du helper

--- app/helpers/characters_helper.rb
+++ app/helpers/characters_helper.rb
@@ -28,10 +28,10 @@ module CharactersHelper
     feature
   end
 
-  def count_total_skill_value(carac_half_level, racial_bonus, s)
+  def count_total_skill_value(carac_half_level, racial_bonus, s, klass_formations_choices)
     total = carac_half_level + racial_bonus
     total += 5 if free_required_skill(s)
-    total += @klass_formations_choices.send(s) unless free_required_skill(s)
+    total += klass_formations_choices.send(s) unless free_required_skill(s)
     total
   end
 end
