10937a834627bcae1e6b228dde9ca86cd2d37262
Eric <eric.le.guellaut@hotmail.fr>
2018-04-15 12:00:20 +0200

    Fonction pour identifier les prérequis toutes classes ...

--- app/models/klass.rb
+++ app/models/klass.rb
@@ -12,8 +12,33 @@ class Klass < ActiveRecord::Base
   #has_and_belongs_to_many :available_skills, :class_name => "Skill", :join_table => :available_skills_for_klass
   has_and_belongs_to_many :required_skills, :class_name => "Skill", :join_table => :required_skills_for_klass
 
+  ARCANIC_KLASS = ["Barde", "Ensorceleur", "Façonneur", "Mage lames", "Magicien", "Sorcier"]
+  DIVINE_KLASS = ["Invocateur", "Paladin", "Prêtre", "Prêtre des runes", "Vengeur"]
+  MARTIAL_KLASS = ["Guerrier", "Maître de guerre", "Rôdeur", "Voleur"]
+  PRIMAL_KLASS = ["Barbare", "Druide", "Gardien", "Limier", "Shaman"]
+  PSIONIC_KLASS = ["Batailleur", "Flamboyant", "Moine", "Psion"]
+
   GRANT_FORMATION_SKILL = ["Barde", "Druide", "Ensorceleur", "Façonneur", "Gardien", "Invocateur", "Limier", "Mage lames", "Magicien", "Paladin", "Prêtre", "Prêtre des runes", "Rôdeur", "Shaman", "Vengeur", "Voleur"]
   GRANT_ONLY_ONE_FORMATION_SKILL = ["Barde", "Druide", "Ensorceleur", "Façonneur", "Gardien", "Invocateur", "Limier", "Mage lames", "Magicien", "Paladin", "Prêtre", "Prêtre des runes", "Shaman", "Vengeur"]
+
+  def self.grouped_klass(klasses)
+    klasses_to_s = ""
+    if ARCANIC_KLASS.all? { |i| (klasses).include?(i) }
+      klasses_to_s = "toute classe arcanique"
+    elsif DIVINE_KLASS.all? { |i| klasses.include?(i) }
+      klasses_to_s = "toute classe divine"
+    elsif MARTIAL_KLASS.all? { |i| klasses.include?(i) }
+      klasses_to_s = "toute classe martiale"
+    elsif PRIMAL_KLASS.all? { |i| klasses.include?(i) }
+      klasses_to_s = "toute classe primale"
+    elsif PSIONIC_KLASS.all? { |i| klasses.include?(i) }
+      klasses_to_s = "toute classe psionique"
+    else
+      klasses_to_s = klasses.join(' ou ')
+    end
+    klasses_to_s
+  end
+
   def is_divine?
     self.power_source == 'divine'
   end
