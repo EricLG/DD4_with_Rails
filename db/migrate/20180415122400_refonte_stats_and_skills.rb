f6d70694770b6f0142178bd79f913383a2aeb257
Eric <eric.le.guellaut@hotmail.fr>
2018-04-28 10:41:49 +0200

    Ajout de valeur par defaut dans la base

--- db/migrate/20180415122400_refonte_stats_and_skills.rb
+++ db/migrate/20180415122400_refonte_stats_and_skills.rb
@@ -21,20 +21,20 @@ class RefonteStatsAndSkills < ActiveRecord::Migration
       t.belongs_to :character, index: true
       t.belongs_to :ability, index: true
       # Columns
-      t.integer :total_value
-      t.integer :initial_value
-      t.integer :bonus_racial
-      t.integer :bonus_klass
-      t.integer :level_4
-      t.integer :level_8
-      t.integer :level_11
-      t.integer :level_14
-      t.integer :level_18
-      t.integer :level_21
-      t.integer :level_24
-      t.integer :level_28
-      t.integer :bonus_parangon
-      t.integer :bonus_epic
+      t.integer :total_value, default: 0
+      t.integer :initial_value, default: 0
+      t.integer :bonus_racial, default: 0
+      t.integer :bonus_klass, default: 0
+      t.integer :level_4, default: 0
+      t.integer :level_8, default: 0
+      t.integer :level_11, default: 0
+      t.integer :level_14, default: 0
+      t.integer :level_18, default: 0
+      t.integer :level_21, default: 0
+      t.integer :level_24, default: 0
+      t.integer :level_28, default: 0
+      t.integer :bonus_parangon, default: 0
+      t.integer :bonus_epic, default: 0
     end
   end
 
