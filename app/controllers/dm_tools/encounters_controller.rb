7f0fc1b914027f399e576c32ebb181e87e015c5d
Le Guellaut <eric.leguellaut_ssii@i-bp.fr>
2018-12-21 11:35:09 +0100

    Suppression variable encounters_exemple

--- app/controllers/dm_tools/encounters_controller.rb
+++ app/controllers/dm_tools/encounters_controller.rb
@@ -14,7 +14,6 @@ class DmTools::EncountersController < ApplicationController
   # GET /encounters/new
   def new
     @encounter = Encounter.new
-    @encounters_exemple = []
     @search = MonsterSearch.new(params[:monster_search])
     @monsters = @search.build_search.order(level: :asc, name: :asc)
     @categories = Monster.categories
