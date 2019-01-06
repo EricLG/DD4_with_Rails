c16c74b94688deade2a9874cce6f3d106bb085c6
Eric <eric.le.guellaut@hotmail.fr>
2019-01-06 19:48:38 +0100

    Ajout link pour modifier une partie

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,8 @@ class Game < ActiveRecord::Base
   has_and_belongs_to_many :characters
   has_one :game_master, through: :campaign
   has_many :comments, dependent: :destroy
+
+  def owned?(current_user)
+    self.user == current_user
+  end
 end
