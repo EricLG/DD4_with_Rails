5ec271734678f5d20291dd28c0cdf6b5d31fe8af
Eric <eric.le.guellaut@hotmail.fr>
2019-02-01 22:30:23 +0100

    Ajout d'un helper pour factoriser les tooltips

--- app/helpers/characters_helper.rb
+++ app/helpers/characters_helper.rb
@@ -5,4 +5,19 @@ module CharactersHelper
     modifier = "+#{modifier}" if modifier > 0
     modifier
   end
+
+  # https://getbootstrap.com/docs/3.4/javascript/#tooltips-options
+  def tooltip(title, description, options = { placement: 'top', trigger: 'hover' }, &block)
+    description = description.blank? ? 'Pas de description' : description
+    html_tooltip = "<span
+                    data-toggle=\"popover\"
+                    data-html=\"true\"
+                    data-placement=\"#{options[:placement]}\"
+                    data-trigger=\"#{options[:trigger]}\"
+                    data-content=\"#{simple_format description}\"
+                    data-title=\"#{title}\" >
+                      #{capture(&block)}
+                    </span>"
+    html_tooltip.html_safe
+  end
 end
