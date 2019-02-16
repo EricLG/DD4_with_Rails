3dc33aba355f7828c5b06cb1e094dec0db689131
Eric <eric.le.guellaut@hotmail.fr>
2019-02-16 12:03:54 +0100

    Utilisation de bootstrap pour changer de type de commentaire

--- app/assets/javascripts/campaign.js
+++ app/assets/javascripts/campaign.js
@@ -1,11 +1,4 @@
 $(document).on("page:change", function() {
-    $(".comment-nav").click(function(e) {
-        e.preventDefault();
-        $(".comment-nav").removeClass('active');
-        $(".comment-content").addClass('hidden');
-        $(this).addClass('active');
-        $("#content-" + $(this).attr("id")).removeClass('hidden');
-    });
 
     $("#js-radio-comment").click(function(e) {
         if ($("#comment_roleplay_rp").prop("checked")) {
