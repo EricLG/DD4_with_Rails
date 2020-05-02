0f946ebbc14d76b71c1946fdc03a0866150068d6
Eric Le Guellaut <eric.le.guellaut@hotmail.fr>
2020-05-02 19:33:23 +0200

    Show random items in welcome page

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,7 +1,10 @@
 class WelcomesController < ApplicationController
   layout :no_sidebloc
 
-  def index; end
+  def index
+    @random_items = MagicItem.all.sample(2)
+    # @random_feats = Feat.all.sample(4)
+  end
 
   def no_sidebloc
     'no_sidebloc' if @current_user
