08468a73d042cb39ae24e5fab7c66a561f7a82a6
Eric Le Guellaut <eric.le.guellaut@hotmail.fr>
2020-05-08 23:12:58 +0200

    deactivate random items on home page

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -2,7 +2,7 @@ class WelcomesController < ApplicationController
   layout :no_sidebloc
 
   def index
-    @random_items = MagicItem.all.sample(2)
+    # @random_items = MagicItem.all.sample(2)
     # @random_feats = Feat.all.sample(4)
   end
 
