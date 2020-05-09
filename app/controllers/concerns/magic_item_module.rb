27d5c10e069eaaee5419cab6860a5113dc74dfb0
Eric Le Guellaut <eric.le.guellaut@hotmail.fr>
2020-05-09 19:14:48 +0200

    Update show object item level template

--- app/controllers/concerns/magic_item_module.rb
+++ app/controllers/concerns/magic_item_module.rb
@@ -1,17 +1,28 @@
 module MagicItemModule
   extend ActiveSupport::Concern
 
-  def search_show_relation(item)
+  def search_show_relation(item, one_group_level = false)
     item_level = item.object_levels.order(:level)
-    left_level_group, right_level_group = item_level.in_groups(2)
-    item_hash = {
-      item: item,
-      item_armor_categories: item.armor_categories.map(&:name),
-      item_weapon_groups: item.weapon_groups.order(:name).map(&:name),
-      item_level_min: item_level.level_min,
-      left_level_group: left_level_group,
-      right_level_group: right_level_group
-    }
+    if one_group_level
+      item_hash = {
+        item: item,
+        item_armor_categories: item.armor_categories.map(&:name),
+        item_weapon_groups: item.weapon_groups.order(:name).map(&:name),
+        item_level_min: item_level.level_min,
+        left_level_group: item_level,
+        right_level_group: []
+      }
+    else
+      left_level_group, right_level_group = item_level.in_groups(2)
+      item_hash = {
+        item: item,
+        item_armor_categories: item.armor_categories.map(&:name),
+        item_weapon_groups: item.weapon_groups.order(:name).map(&:name),
+        item_level_min: item_level.level_min,
+        left_level_group: left_level_group,
+        right_level_group: right_level_group
+      }
+    end
     item_hash
   end
 end
