02aef165a00bf319ada3b34049b517823f755e88
Eric Le Guellaut <eric.le.guellaut@hotmail.fr>
2020-05-20 21:30:45 +0200

    Fix order of races and classes

--- app/controllers/klasses_controller.rb
+++ app/controllers/klasses_controller.rb
@@ -2,11 +2,11 @@ class KlassesController < ApplicationController
   layout 'no_sidebloc', only: :index
 
   def index
-    @klasses = Klass.select(:id, :name, :role).all.group_by(&:role)
+    @klasses = Klass.select(:id, :name, :role).all.order(:name).group_by(&:role)
   end
 
   def show
-    @klasses = Klass.select(:id, :name).all
+    @klasses = Klass.select(:id, :name).all.order(:name)
     @klass = Klass.includes(:features, :source).find_by_id(params[:id])
   end
 end
--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -2,11 +2,11 @@ class RacesController < ApplicationController
   layout 'no_sidebloc', only: :index
 
   def index
-    @races = Race.select(:id, :name).all
+    @races = Race.select(:id, :name).all.order(:name)
   end
 
   def show
-    @races = Race.select(:id, :name).all
+    @races = Race.select(:id, :name).all.order(:name)
     @race = Race.includes(:features, :source).find_by_id(params[:id])
   end
 end
