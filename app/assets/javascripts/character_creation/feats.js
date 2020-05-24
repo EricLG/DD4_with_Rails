46d2c67440ad70990ce44b366a449d6949ba5308
Eric Le Guellaut <eric.le.guellaut@hotmail.fr>
2020-05-24 16:43:42 +0200

    Prevent feat choice without required conditional info

--- app/assets/javascripts/character_creation/feats.js
+++ app/assets/javascripts/character_creation/feats.js
@@ -1,12 +1,19 @@
 $(document).on("turbolinks:load", function() {
   $("#character_chosen_feats_attributes_0_feat_id").on("change", function(e) {
-    var feats_with_add_choice = ['Apprentissage', 'Arme de prédilection', 'Compétence de prédilection',
-    'Expertise des focaliseurs', 'Expertise aux armes', 'Linguiste',
-    'Maîtrise polyvalente', "Maniement d'arme"]
+    var feats_with_add_choice = [
+      'Apprentissage',
+      'Arme de prédilection',
+      'Compétence de prédilection',
+      'Expertise des focaliseurs',
+      'Expertise aux armes',
+      'Linguiste',
+      'Maîtrise polyvalente',
+      "Maniement d'arme"
+    ]
 
     if($(this).val()) {
       $.ajax({
-        url: "/feats//" + $(this).val() + "/feat_avantage",
+        url: "/feats/" + $(this).val() + "/feat_avantage",
         success: function(data, textStatus, jqHhr) {
           $("#resume").removeClass("hidden");
           $("#feat_cat").text(data.category)
@@ -19,6 +26,8 @@ $(document).on("turbolinks:load", function() {
           $("#languages").addClass("hidden");
           $("#proficiency").addClass("hidden");
           $("#implement").addClass("hidden");
+
+          $('.js-optional-select').attr('disabled', true)
 
           if (feats_with_add_choice.includes(data.name)) {
             checkForAdditionalDataToFeat(data)
@@ -38,6 +47,7 @@ $(document).on("turbolinks:load", function() {
   function checkForAdditionalDataToFeat(feat) {
     if (feat.name === "Apprentissage") {
       $("#apprentissage").removeClass("hidden");
+      $( ".select-apprentissage" ).attr('disabled', false)
       $( ".select-apprentissage" ).select2({
         theme: "bootstrap",
         placeholder: "Choisissez une compétence...",
@@ -46,6 +56,7 @@ $(document).on("turbolinks:load", function() {
     }
     if (feat.name === "Arme de prédilection" || feat.name === "Expertise aux armes" || feat.name === "Maîtrise polyvalente") {
       $("#weapon-group").removeClass("hidden");
+      $( ".select-weapon-group" ).attr('disabled', false)
       $( ".select-weapon-group" ).select2({
         theme: "bootstrap",
         placeholder: "Choisissez un groupe d'arme...",
@@ -54,6 +65,7 @@ $(document).on("turbolinks:load", function() {
     }
     if (feat.name === "Expertise des focaliseurs" || feat.name === "Maîtrise polyvalente") {
       $("#implement").removeClass("hidden");
+      $( ".select-implement" ).attr('disabled', false)
       $( ".select-implement" ).select2({
         theme: "bootstrap",
         placeholder: "Choisissez un type de focaliseur...",
@@ -62,6 +74,7 @@ $(document).on("turbolinks:load", function() {
     }
     if (feat.name === "Compétence de prédilection") {
       $("#predilection-skill").removeClass("hidden");
+      $( ".select-predilection-skill" ).attr('disabled', false)
       $( ".select-predilection-skill" ).select2({
         theme: "bootstrap",
         placeholder: "Choisissez une compétence...",
@@ -70,6 +83,7 @@ $(document).on("turbolinks:load", function() {
     }
     if (feat.name === 'Linguiste') {
       $("#languages").removeClass("hidden");
+      $( ".select-language" ).attr('disabled', false)
       $( ".select-language" ).select2({
         theme: "bootstrap",
         placeholder: "Choisissez trois langues...",
@@ -79,6 +93,7 @@ $(document).on("turbolinks:load", function() {
     }
     if (feat.name === "Maniement d'arme") {
       $("#proficiency").removeClass("hidden");
+      $( ".select-proficiency" ).attr('disabled', false)
       $( ".select-proficiency" ).select2({
         theme: "bootstrap",
         placeholder: "Choisissez une arme...",
