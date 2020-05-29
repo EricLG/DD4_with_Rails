5c3235416d13a818a0aedb5b6ce4de509bc8ce73
Eric Le Guellaut <eric.le.guellaut@hotmail.fr>
2020-05-29 21:27:47 +0200

    Add welcome image

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,10 +1,7 @@
 class WelcomesController < ApplicationController
   layout :no_sidebloc
 
-  def index
-    # @random_items = MagicItem.all.sample(2)
-    # @random_feats = Feat.all.sample(4)
-  end
+  def index; end
 
   def no_sidebloc
     'no_sidebloc' if @current_user
